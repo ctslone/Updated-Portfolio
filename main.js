41f0c3ba37d5e09c27c46ecdf0d138b6adb110d4
ctslone <ctslone27@gmail.com>
2019-11-07 10:10:07 -0500

    adding connect icons for GB linkedin and email. added mdal for email contact. fixed project card issue when viewing on medium screen. fixed connect icon issue when on small screen. changing body color.adding a subtle zoom to the project images. adding links to project images. adding footer and making it appear from bottom upon scroll to bottom

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+$(document).ready( function() {
+
+
 $('.navTrigger').click(function () {
     $(this).toggleClass('active');
-    console.log("Clicked menu");
+    // console.log("Clicked menu");
     $("#mainListDiv").toggleClass("show_list");
     $("#mainListDiv").fadeIn();
 
@@ -10,12 +13,29 @@ $("#about-btn").click(function() {
     $('html,body').animate({
         scrollTop: $("#about-div").offset().top -95},
         'slow');
-    console.log("scroll")
+    // console.log("scroll")
 });
 
 $("#project-btn").click(function() {
     $('html,body').animate({
         scrollTop: $("#projects-div").offset().top -95},
         'slow');
-    console.log("scroll")
+    // console.log("scroll")
 });
+
+$("#connect-btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#connect-div").offset().top -80},
+        'slow');
+    // console.log("scroll")
+});
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+        $('footer').slideDown(300);
+    } else {
+        $('footer').slideUp(300);
+    }
+});
+
+})
