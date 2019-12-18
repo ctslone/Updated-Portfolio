5ca71cf68491af9910235a7eb6bdfa0cb593471e
ctslone <ctslone27@gmail.com>
2019-12-18 18:36:28 -0500

    removing unwanted console logs

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ $(document).ready( function() {
 
 $('.navTrigger').click(function () {
     $(this).toggleClass('active');
-    // console.log("Clicked menu");
     $("#mainListDiv").toggleClass("show_list");
     $("#mainListDiv").fadeIn();
 
@@ -13,21 +12,18 @@ $("#about-btn").click(function() {
     $('html,body').animate({
         scrollTop: $("#about-div").offset().top -95},
         'slow');
-    // console.log("scroll")
 });
 
 $("#project-btn").click(function() {
     $('html,body').animate({
         scrollTop: $("#projects-div").offset().top -95},
         'slow');
-    // console.log("scroll")
 });
 
 $("#connect-btn").click(function() {
     $('html,body').animate({
         scrollTop: $("#connect-div").offset().top -80},
         'slow');
-    // console.log("scroll")
 });
 
 $(window).scroll(function () {
