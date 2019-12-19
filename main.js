1376e74ae829fbc34b8f99a22c79450e134fedc4
ctslone <ctslone27@gmail.com>
2019-12-19 16:25:20 -0500

    reorganizing file structure. adding headshot in navbar. updating resume. messing with main page view heights. still cant decide how big the code should be

--- main.js
+++ main.js
@@ -1,37 +1,49 @@
-$(document).ready( function() {
+$(document).ready(function () {
 
+    // fade nav effect
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 50) {
+            $('.nav').addClass('affix');
+        } else {
+            $('.nav').removeClass('affix');
+        }
+    });
 
-$('.navTrigger').click(function () {
-    $(this).toggleClass('active');
-    $("#mainListDiv").toggleClass("show_list");
-    $("#mainListDiv").fadeIn();
+    // shrink nav to mobile view
+    $('.navTrigger').click(function () {
+        $(this).toggleClass('active');
+        $("#mainListDiv").toggleClass("show_list");
+        $("#mainListDiv").fadeIn();
 
-});
-
-$("#about-btn").click(function() {
-    $('html,body').animate({
-        scrollTop: $("#about-div").offset().top -95},
-        'slow');
-});
-
-$("#project-btn").click(function() {
-    $('html,body').animate({
-        scrollTop: $("#projects-div").offset().top -95},
-        'slow');
-});
-
-$("#connect-btn").click(function() {
-    $('html,body').animate({
-        scrollTop: $("#connect-div").offset().top -80},
-        'slow');
-});
-
-$(window).scroll(function () {
-    if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-        $('footer').slideDown(300);
-    } else {
-        $('footer').slideUp(300);
-    }
-});
+    });
+    // scroll to top of about section
+    $("#about-btn").click(function () {
+        $('html,body').animate({
+            scrollTop: $("#about-div").offset().top - 95
+        },
+            'slow');
+    });
+    // scroll to top of projects
+    $("#project-btn").click(function () {
+        $('html,body').animate({
+            scrollTop: $("#projects-div").offset().top - 95
+        },
+            'slow');
+    });
+    // scroll to top of connect
+    $("#connect-btn").click(function () {
+        $('html,body').animate({
+            scrollTop: $("#connect-div").offset().top - 80
+        },
+            'slow');
+    });
+    // footer scroll animations
+    $(window).scroll(function () {
+        if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+            $('footer').slideDown(300);
+        } else {
+            $('footer').slideUp(300);
+        }
+    });
 
 })
